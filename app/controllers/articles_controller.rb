9f9f1b9a163cd7ce8743d7cf1891aad284cf9ca1
Sergio Mendez <sergio.mendez.roca@gmail.com>
2023-05-25 16:30:17 -0700

    created private method for new article params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -21,7 +21,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  
-
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
   
 end
